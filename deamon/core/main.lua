08c89edeb759e92e5f6656a64dbfc2df874fd31b
MisterNoNameLP <acc@namelessys.de>
2023-04-13 19:32:04 +0200

    uodate dams

--- deamon/core/main.lua
+++ deamon/core/main.lua
@@ -1,6 +1,6 @@
 --LÖVE main file 
 --this version is based on v0.19.1
-local version = "1.0-prerelease-43d"
+local version = "1.0-prerelease-44"
 
 function love.load(args)
 	print("--===== Starting DAMS v" .. tostring(version) .. " =====--")
